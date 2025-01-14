a12444e2737ecf399d845dc70009b47f1cdbec1a
Eduard Frigola <eduardfrigola@gmail.com>
2025-01-14 20:30:52 +0100

    [ScStart] Reboot server automatically if crashed

--- src/scStart.h
+++ src/scStart.h
@@ -32,57 +32,61 @@ public:
     }
       
     void threadedFunction(){
-        string termcmd = scPath;
-        termcmd += " -u " + ofToString(prefs.udpPort);
-        termcmd += " -B " + prefs.bindAddress;
-        termcmd += " -c " + ofToString(prefs.numControlBusChannels);
-        termcmd += " -a " + ofToString(prefs.numAudioBusChannels);
-        termcmd += " -i " + ofToString(prefs.numInputBusChannels);
-        termcmd += " -o " + ofToString(prefs.numOutputBusChannels);
-        termcmd += " -z " + ofToString(prefs.blockSize);
-        termcmd += " -Z " + ofToString(prefs.hardwareBufferSize);
-        termcmd += " -S " + ofToString(prefs.hardwareSampleRate);
-        termcmd += " -b " + ofToString(prefs.numBuffers);
-        termcmd += " -n " + ofToString(prefs.maxNodes);
-        termcmd += " -d " + ofToString(prefs.maxSynthDefs);
-        termcmd += " -m " + ofToString(prefs.memSize);
-        termcmd += " -w " + ofToString(prefs.numWireBufs);
-        termcmd += " -r " + ofToString(prefs.numRGens);
-        termcmd += " -l " + ofToString(prefs.maxLogins);
-        termcmd += " -s " + ofToString(prefs.safetyClipThreshold);
-        termcmd += " -H " + prefs.deviceName;
-        if(prefs.ugensPlugins != "") termcmd += " -U " + ofToString("\"") + ofToDataPath(prefs.ugensPlugins, true) + ofToString(":") + ofToDataPath("Supercollider/scsynth/aarch/bin", true) + ofToString("\"");
- 
-        FILE * ret = nullptr;
-    #ifdef TARGET_WIN32
-        ret = _popen(termcmd.c_str(),"r");
-    #else
-        ret = popen(termcmd.c_str(),"r");
-    #endif
-
-        string strret;
-        int c;
-
-        if (ret == nullptr){
-            ofLogError("ofUtils") << "ofSystem(): error opening return file for command \"" << termcmd  << "\"";
-        }else{
-            c = fgetc (ret);
-            string line;
-            while (c != EOF) {
-                strret += c;
+        while(reboot){
+            string termcmd = scPath;
+            termcmd += " -u " + ofToString(prefs.udpPort);
+            termcmd += " -B " + prefs.bindAddress;
+            termcmd += " -c " + ofToString(prefs.numControlBusChannels);
+            termcmd += " -a " + ofToString(prefs.numAudioBusChannels);
+            termcmd += " -i " + ofToString(prefs.numInputBusChannels);
+            termcmd += " -o " + ofToString(prefs.numOutputBusChannels);
+            termcmd += " -z " + ofToString(prefs.blockSize);
+            termcmd += " -Z " + ofToString(prefs.hardwareBufferSize);
+            termcmd += " -S " + ofToString(prefs.hardwareSampleRate);
+            termcmd += " -b " + ofToString(prefs.numBuffers);
+            termcmd += " -n " + ofToString(prefs.maxNodes);
+            termcmd += " -d " + ofToString(prefs.maxSynthDefs);
+            termcmd += " -m " + ofToString(prefs.memSize);
+            termcmd += " -w " + ofToString(prefs.numWireBufs);
+            termcmd += " -r " + ofToString(prefs.numRGens);
+            termcmd += " -l " + ofToString(prefs.maxLogins);
+            termcmd += " -s " + ofToString(prefs.safetyClipThreshold);
+            termcmd += " -H " + prefs.deviceName;
+            //        if(prefs.ugensPlugins != "") termcmd += " -U " + ofToString("\"") + ofToDataPath(prefs.ugensPlugins, true).string() + ofToString(":") + ofToDataPath("Supercollider/scsynth/aarch/bin", true).string() + ofToString("\"");
+            if(prefs.ugensPlugins != "") termcmd += " -U " + ofToString("\"") + ofToDataPath(prefs.ugensPlugins, true) + ofToString(":") + ofToDataPath("Supercollider/scsynth/aarch/bin", true) + ofToString("\"");
+            
+            
+#ifdef TARGET_WIN32
+            ret = _popen(termcmd.c_str(),"r");
+#else
+            ret = popen(termcmd.c_str(),"r");
+#endif
+            
+            string strret;
+            int c;
+            
+            if (ret == nullptr){
+                ofLogError("ofUtils") << "ofSystem(): error opening return file for command \"" << termcmd  << "\"";
+            }else{
                 c = fgetc (ret);
-                if(c != '\n'){
-                    line += c;
-                }else{
-                    ofLog() << line;
-                    line = "";
+                string line;
+                while (c != EOF) {
+                    strret += c;
+                    c = fgetc (ret);
+                    if(c != '\n'){
+                        line += c;
+                    }else{
+                        ofLog() << line;
+                        line = "";
+                    }
                 }
+#ifdef TARGET_WIN32
+                _pclose (ret);
+#else
+                pclose (ret);
+#endif
+                ret = nullptr;
             }
-    #ifdef TARGET_WIN32
-            _pclose (ret);
-    #else
-            pclose (ret);
-    #endif
         }
     }
       
@@ -92,12 +96,16 @@ public:
     }
     
     void killServer(){
+        reboot = false;
         ofSystem("killall scsynth");
     }
     
 private:
     string scPath = "";
     scPreferences prefs;
+    bool reboot = true;
+    
+    FILE * ret = nullptr;
 };
 
 #endif /* scStart_h */
