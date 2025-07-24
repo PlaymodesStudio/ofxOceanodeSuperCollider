9be0843d50e3ce288f906eb97dcf057023528870
santiago vilanova <santiagovilanova@MacBook-Pro-de-santiago-2.local>
2025-07-24 17:40:15 +0200

    [serverManager]: added vst event listeners

--- src/serverManager.cpp
+++ src/serverManager.cpp
@@ -15,317 +15,334 @@
 #include "ofxOceanodeShared.h"
 
 serverManager::serverManager(){
-    volume = 1;
-    mute = false;
-    delay = 0;
-    stereomix = false;
-    stereomixSize = 2;
-    audioDevice = 0;
-    dumpOsc = false;
-    numRecomputeGraphOnce = 0;
+	volume = 1;
+	mute = false;
+	delay = 0;
+	stereomix = false;
+	stereomixSize = 2;
+	audioDevice = 0;
+	dumpOsc = false;
+	numRecomputeGraphOnce = 0;
 };
 
 serverManager::~serverManager(){
-    for(auto node : nodesList) node->free(server);
-    nodesList.clear();
-    for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
-    busses.clear();
+	for(auto node : nodesList) node->free(server);
+	nodesList.clear();
+	for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
+	busses.clear();
 }
 
 void serverManager::setup(){
-    server = new ofxSCServer(preferences.bindAddress, preferences.udpPort);
-    if(preferences.local){
-        sc = new scStart(preferences);
-    }
-    boot();
-    
-    listeners.push(ofxOceanodeShared::getPresetWillBeLoadedEvent().newListener([this](){
-        for(auto node : nodesList) node->free(server);
-        nodesList.clear();
-        for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
-        busses.clear();
-    }));
-    
-    listeners.push(ofxOceanodeShared::getPresetHasLoadedEvent().newListener([this](){
-        recomputeGraph();
-    }));
-    
-    listeners.push(server->serverBootedEvent.newListener([this](){
-        initialize();
-    }));
-    listeners.push(server->serverInitializedEvent.newListener([this](){
-        recomputeGraph();
-    }));
+	server = new ofxSCServer(preferences.bindAddress, preferences.udpPort);
+	if(preferences.local){
+		sc = new scStart(preferences);
+	}
+	boot();
+	
+	listeners.push(ofxOceanodeShared::getPresetWillBeLoadedEvent().newListener([this](){
+		for(auto node : nodesList) node->free(server);
+		nodesList.clear();
+		for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
+		busses.clear();
+	}));
+	
+	listeners.push(ofxOceanodeShared::getPresetHasLoadedEvent().newListener([this](){
+		recomputeGraph();
+	}));
+	
+	listeners.push(server->serverBootedEvent.newListener([this](){
+		initialize();
+	}));
+	listeners.push(server->serverInitializedEvent.newListener([this](){
+		recomputeGraph();
+	}));
+	
+	// Subscribe to VST events from server and forward them
+	listeners.push(server->vstParameterChanged.newListener(this, &serverManager::onVSTParameterChanged));
+	listeners.push(server->vstParameterAutomated.newListener(this, &serverManager::onVSTParameterAutomated));
+	listeners.push(server->vstPluginOpened.newListener([this](VSTPluginOpenedEvent& event) {
+		vstPluginOpened.notify(event);
+	}));
+}
+
+void serverManager::onVSTParameterChanged(VSTParameterEvent& event) {
+	// Forward the event to any listening nodes
+	vstParameterChanged.notify(event);
+}
+
+void serverManager::onVSTParameterAutomated(VSTParameterEvent& event) {
+	// Forward the event to any listening nodes
+	vstParameterAutomated.notify(event);
 }
 
 void serverManager::draw(){
-    if(ImGui::Button("Boot Server")){
-        boot();
-    }
-    
-    ImGui::SameLine();
-    if(ImGui::Button("Kill Server")){
-        if(preferences.local){
-            kill();
-        }
-    }
-    
-    ImGui::SameLine();
-    
-    if(ImGui::Checkbox("Local", &preferences.local)){
-        if(!preferences.local){
-            sc->killServer();
-            delete sc;
-        }else{
-            sc = new scStart(preferences);
-        }
-    }
-    
-    if(ImGui::Button("Load Defs")){
-        loadDefs();
-    }
-    
-    ImGui::Separator();
-    
-    if(ImGui::SliderFloat("Volume", &volume, 0, 1)){
-        setVolume(volume);
-    }
-    
-    ImGui::SameLine();
-    
-    if(ImGui::Checkbox("Mute", &mute)){
-        if(mute) setVolume(0);
-        else setVolume(volume);
-    }
-    
-    if(ImGui::SliderInt("Delay", &delay, 0, 5000)){
-        setDelay(delay);
-    }
-    
-    ImGui::Separator();
-    
-    ImGui::InputInt("Udp Port", &preferences.udpPort);
-    
-    
-    int intaddress[4] = {0, 0, 0, 0};
-    vector<string> splitAddress = ofSplitString(preferences.bindAddress, ".");
-    for(int i = 0; i < 4; i++){
-        intaddress[i] = ofToInt(splitAddress[i]);
-    }
-    if(ImGui::InputInt4("Bind Address", &intaddress[0])){
-        string newAddress = ofToString(intaddress[0]) + "."
-                            + ofToString(intaddress[1]) + "."
-                            + ofToString(intaddress[2]) + "."
-                            + ofToString(intaddress[3]) + ".";
-        preferences.bindAddress = newAddress;
-    }
-    
-    ImGui::InputInt("Audio Busses", &preferences.numAudioBusChannels);
-    ImGui::InputInt("Control Busses", &preferences.numControlBusChannels);
-    ImGui::InputInt("Input Channels", &preferences.numInputBusChannels);
-    ImGui::InputInt("Output Channels", &preferences.numOutputBusChannels);
-    ImGui::InputInt("Block Size", &preferences.blockSize);
-    ImGui::InputInt("Buffer Size", &preferences.hardwareBufferSize);
-    ImGui::InputInt("Sampling Rate", &preferences.hardwareSampleRate);
-    ImGui::InputInt("Num Buffers", &preferences.numBuffers);
-    ImGui::InputInt("Max Nodes", &preferences.maxNodes);
-    ImGui::InputInt("Max Synthdefs", &preferences.maxSynthDefs);
-    ImGui::InputInt("Mem Size", &preferences.memSize);
-    ImGui::InputInt("Num Wire Bufs", &preferences.numWireBufs);
-    ImGui::InputInt("Num R Gens", &preferences.numRGens);
-    ImGui::InputInt("Max Logins", &preferences.maxLogins);
-    ImGui::InputFloat("Safety Clip Th", &preferences.safetyClipThreshold);
-    
-    auto vector_getter = [](void* vec, int idx, const char** out_text)
-    {
-        auto& vector = *static_cast<std::vector<std::string>*>(vec);
-        if (idx < 0 || idx >= static_cast<int>(vector.size())) { return false; }
-        *out_text = vector.at(idx).c_str();
-        return true;
-    };
-    
-    if(ImGui::Combo("Audio Device", &audioDevice, vector_getter, static_cast<void*>(&audioDeviceNames), audioDeviceNames.size())){
-        if(audioDevice == 0){
-            preferences.deviceName = "nil";
-        }else{
-            preferences.deviceName = audioDeviceNames[audioDevice];
-        }
-    }
-    
-    //Device name;
-    bool verb = preferences.verbosity;
-    if(ImGui::Checkbox("Verbosity", &verb)){
-        if(verb) preferences.verbosity = 1;
-        else preferences.verbosity = 0;
-    }
+	if(ImGui::Button("Boot Server")){
+		boot();
+	}
+	
+	ImGui::SameLine();
+	if(ImGui::Button("Kill Server")){
+		if(preferences.local){
+			kill();
+		}
+	}
+	
+	ImGui::SameLine();
+	
+	if(ImGui::Checkbox("Local", &preferences.local)){
+		if(!preferences.local){
+			sc->killServer();
+			delete sc;
+		}else{
+			sc = new scStart(preferences);
+		}
+	}
+	
+	if(ImGui::Button("Load Defs")){
+		loadDefs();
+	}
+	
+	ImGui::Separator();
+	
+	if(ImGui::SliderFloat("Volume", &volume, 0, 1)){
+		setVolume(volume);
+	}
+	
+	ImGui::SameLine();
+	
+	if(ImGui::Checkbox("Mute", &mute)){
+		if(mute) setVolume(0);
+		else setVolume(volume);
+	}
+	
+	if(ImGui::SliderInt("Delay", &delay, 0, 5000)){
+		setDelay(delay);
+	}
+	
+	ImGui::Separator();
+	
+	ImGui::InputInt("Udp Port", &preferences.udpPort);
+	
+	
+	int intaddress[4] = {0, 0, 0, 0};
+	vector<string> splitAddress = ofSplitString(preferences.bindAddress, ".");
+	for(int i = 0; i < 4; i++){
+		intaddress[i] = ofToInt(splitAddress[i]);
+	}
+	if(ImGui::InputInt4("Bind Address", &intaddress[0])){
+		string newAddress = ofToString(intaddress[0]) + "."
+							+ ofToString(intaddress[1]) + "."
+							+ ofToString(intaddress[2]) + "."
+							+ ofToString(intaddress[3]) + ".";
+		preferences.bindAddress = newAddress;
+	}
+	
+	ImGui::InputInt("Audio Busses", &preferences.numAudioBusChannels);
+	ImGui::InputInt("Control Busses", &preferences.numControlBusChannels);
+	ImGui::InputInt("Input Channels", &preferences.numInputBusChannels);
+	ImGui::InputInt("Output Channels", &preferences.numOutputBusChannels);
+	ImGui::InputInt("Block Size", &preferences.blockSize);
+	ImGui::InputInt("Buffer Size", &preferences.hardwareBufferSize);
+	ImGui::InputInt("Sampling Rate", &preferences.hardwareSampleRate);
+	ImGui::InputInt("Num Buffers", &preferences.numBuffers);
+	ImGui::InputInt("Max Nodes", &preferences.maxNodes);
+	ImGui::InputInt("Max Synthdefs", &preferences.maxSynthDefs);
+	ImGui::InputInt("Mem Size", &preferences.memSize);
+	ImGui::InputInt("Num Wire Bufs", &preferences.numWireBufs);
+	ImGui::InputInt("Num R Gens", &preferences.numRGens);
+	ImGui::InputInt("Max Logins", &preferences.maxLogins);
+	ImGui::InputFloat("Safety Clip Th", &preferences.safetyClipThreshold);
+	
+	auto vector_getter = [](void* vec, int idx, const char** out_text)
+	{
+		auto& vector = *static_cast<std::vector<std::string>*>(vec);
+		if (idx < 0 || idx >= static_cast<int>(vector.size())) { return false; }
+		*out_text = vector.at(idx).c_str();
+		return true;
+	};
+	
+	if(ImGui::Combo("Audio Device", &audioDevice, vector_getter, static_cast<void*>(&audioDeviceNames), audioDeviceNames.size())){
+		if(audioDevice == 0){
+			preferences.deviceName = "nil";
+		}else{
+			preferences.deviceName = audioDeviceNames[audioDevice];
+		}
+	}
+	
+	//Device name;
+	bool verb = preferences.verbosity;
+	if(ImGui::Checkbox("Verbosity", &verb)){
+		if(verb) preferences.verbosity = 1;
+		else preferences.verbosity = 0;
+	}
 
-    bool localUgens = (preferences.ugensPlugins != "");
-    if(ImGui::Checkbox("LocalUgens", &localUgens)){
-        if(localUgens)
-            preferences.ugensPlugins = ofToDataPath("Supercollider/Ugens", true);
-        else
-            preferences.ugensPlugins = "";
-    }
-    
-    if(ImGui::Checkbox("Dump Osc", &dumpOsc)){
-        ofxOscMessage m;
-        m.setAddress("/dumpOSC");
-        if(dumpOsc) m.addIntArg(1);
-        else m.addIntArg(0);
-        server->sendMsg(m);
-    }
+	bool localUgens = (preferences.ugensPlugins != "");
+	if(ImGui::Checkbox("LocalUgens", &localUgens)){
+		if(localUgens)
+			preferences.ugensPlugins = ofToDataPath("Supercollider/Ugens", true);
+		else
+			preferences.ugensPlugins = "";
+	}
+	
+	if(ImGui::Checkbox("Dump Osc", &dumpOsc)){
+		ofxOscMessage m;
+		m.setAddress("/dumpOSC");
+		if(dumpOsc) m.addIntArg(1);
+		else m.addIntArg(0);
+		server->sendMsg(m);
+	}
 }
 
 void serverManager::boot(){
-    if(preferences.local){
-        sc->start();
-    }
+	if(preferences.local){
+		sc->start();
+	}
 }
 
 void serverManager::initialize(){
-    server->notify();
-    
-    ofxOscMessage m2;
-    m2.setAddress("/g_new");
-    m2.addIntArg(1);
-    m2.addIntArg(0);
-    m2.addIntArg(0);
-    server->sendMsg(m2);
-    
-    loadDefs();
-    
-    setVolume(volume);
-    setDelay(delay);
-    setStereoMix(stereomix);
-    setStereoMixSize(stereomixSize);
+	server->notify();
+	
+	ofxOscMessage m2;
+	m2.setAddress("/g_new");
+	m2.addIntArg(1);
+	m2.addIntArg(0);
+	m2.addIntArg(0);
+	server->sendMsg(m2);
+	
+	loadDefs();
+	
+	setVolume(volume);
+	setDelay(delay);
+	setStereoMix(stereomix);
+	setStereoMixSize(stereomixSize);
 }
 
 void serverManager::kill(){
-    if(preferences.local){
-        ofxOscMessage m;
-        m.setAddress("/quit");
-        server->sendMsg(m);
-        sc->killServer();
-    }
+	if(preferences.local){
+		ofxOscMessage m;
+		m.setAddress("/quit");
+		server->sendMsg(m);
+		sc->killServer();
+	}
 }
 
 void serverManager::loadDefs(){
-    ofxOscMessage m;
-    m.setAddress("/d_loadDir");
-    m.addStringArg(ofToDataPath("Supercollider/Synthdefs", true));
-    m.addIntArg(0);
-    server->sendMsg(m);
+	ofxOscMessage m;
+	m.setAddress("/d_loadDir");
+	m.addStringArg(ofToDataPath("Supercollider/Synthdefs", true));
+	m.addIntArg(0);
+	server->sendMsg(m);
 }
 
 void serverManager::setVolume(float _volume){
-    volume = _volume;
-    for(auto &o : outputs) o->setVolume(volume);
-    
+	volume = _volume;
+	for(auto &o : outputs) o->setVolume(volume);
+	
 }
 
 void serverManager::setDelay(int _delay){
-    delay = _delay;
-    for(auto &o : outputs) o->setDelay(delay);
+	delay = _delay;
+	for(auto &o : outputs) o->setDelay(delay);
 }
 
 void serverManager::setStereoMix(bool _stereomix){
-    stereomix = _stereomix;
-    for(auto &o : outputs) o->setStereoMix(stereomix);
+	stereomix = _stereomix;
+	for(auto &o : outputs) o->setStereoMix(stereomix);
 }
 
 void serverManager::setStereoMixSize(int _stereomixSize){
-    stereomixSize = _stereomixSize;
-    for(auto &o : outputs) o->setStereoMixSize(stereomixSize);
+	stereomixSize = _stereomixSize;
+	for(auto &o : outputs) o->setStereoMixSize(stereomixSize);
 }
 
 void serverManager::addOutput(scOutput *output){
-    outputs.push_back(output);
-    setVolume(volume);
-    setDelay(delay);
-    setStereoMix(stereomix);
-    setStereoMixSize(stereomixSize);
+	outputs.push_back(output);
+	setVolume(volume);
+	setDelay(delay);
+	setStereoMix(stereomix);
+	setStereoMixSize(stereomixSize);
 }
 
 void serverManager::removeOutput(scOutput *output){
-    outputs.erase(std::remove(outputs.begin(), outputs.end(), output), outputs.end());
-    recomputeGraph();
+	outputs.erase(std::remove(outputs.begin(), outputs.end(), output), outputs.end());
+	recomputeGraph();
 }
 
 void serverManager::recomputeGraph(){
-    if(ofxOceanodeShared::isPresetLoading()) return;
+	if(ofxOceanodeShared::isPresetLoading()) return;
 //    ofLog() << "Recompute Graph";
-    if(outputs.size() == 0){
-        for(auto node : nodesList) node->free(server);
-        nodesList.clear();
-        for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
-        busses.clear();
-    }else{
-        server->setBLatency(true);
+	if(outputs.size() == 0){
+		for(auto node : nodesList) node->free(server);
+		nodesList.clear();
+		for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
+		busses.clear();
+	}else{
+		server->setBLatency(true);
 //        server->setWaitToSend(true);
-        
-        //TODO: Only delete non existing nodes
-        for(auto node : nodesList){
-            if(node != nullptr)
-                node->free(server);
-        }
-        nodesList.clear();
-        
+		
+		//TODO: Only delete non existing nodes
+		for(auto node : nodesList){
+			if(node != nullptr)
+				node->free(server);
+		}
+		nodesList.clear();
+		
 //        for(auto &b : busses) b.free();
-        for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
-        busses.clear();
-        outputBussesRefToNode.clear();
-        inputBussesRefToNode.clear();
-        
-        std::vector<scNode*> newNodesList;
-        std::map<scNode*, std::pair<int, std::vector<int>>> nodeChilds;
-        for(int i = 0; i < outputs.size(); i++){
-            if(outputs[i]->isInputConnected()){
-                outputs[i]->getInputNode()->appendOrderedNodes(newNodesList, nodeChilds);
-            }
-        }
-        for(int i = 0; i < outputs.size(); i++){
-            if(outputs[i]->isInputConnected()){
-                newNodesList.push_back(outputs[i]);
-            }
-        }
-            
-            std::map<nodePort, std::vector<scNode*>> connections;
-            
-            for (auto it = newNodesList.rbegin(); it != newNodesList.rend(); ++it) {
-                (*it)->getConnections(connections);
-                (*it)->createSynth(server);
-            }
-                
-        //Create outputBusses for all nodes except scOutput
-        for (auto it = newNodesList.rbegin(); it != newNodesList.rend(); ++it) {
-            for(int i = 0; i < (*it)->getNumOutputs() ; i++){
-                busses.emplace_back(RATE_AUDIO, MAX_NODE_CHANNELS, server);
-                int busindex = busses.back().index;
-                (*it)->setOutputBus(server, i, busindex);
-                outputBussesRefToNode[(*it)][i] = busindex;
-            }
-        }
-        
-            for(auto &c : connections){
-                for(auto &dest : c.second){
-                    int busindex = outputBussesRefToNode[c.first.getNodeRef()][c.first.getIndex()];
-                    dest->setInputBus(server, c.first.getNodeRef(), busindex);
-                    inputBussesRefToNode[dest].push_back(busindex);
-                }
-            }
-        
-        for (auto it = newNodesList.rbegin(); it != newNodesList.rend(); ++it) {
-            (*it)->reassignAudioControls.notify();
-        }
-        
-        nodesList = newNodesList;
+		for(auto b = busses.rbegin(); b != busses.rend(); ++b) b->free();
+		busses.clear();
+		outputBussesRefToNode.clear();
+		inputBussesRefToNode.clear();
+		
+		std::vector<scNode*> newNodesList;
+		std::map<scNode*, std::pair<int, std::vector<int>>> nodeChilds;
+		for(int i = 0; i < outputs.size(); i++){
+			if(outputs[i]->isInputConnected()){
+				outputs[i]->getInputNode()->appendOrderedNodes(newNodesList, nodeChilds);
+			}
+		}
+		for(int i = 0; i < outputs.size(); i++){
+			if(outputs[i]->isInputConnected()){
+				newNodesList.push_back(outputs[i]);
+			}
+		}
+			
+			std::map<nodePort, std::vector<scNode*>> connections;
+			
+			for (auto it = newNodesList.rbegin(); it != newNodesList.rend(); ++it) {
+				(*it)->getConnections(connections);
+				(*it)->createSynth(server);
+			}
+				
+		//Create outputBusses for all nodes except scOutput
+		for (auto it = newNodesList.rbegin(); it != newNodesList.rend(); ++it) {
+			for(int i = 0; i < (*it)->getNumOutputs() ; i++){
+				busses.emplace_back(RATE_AUDIO, MAX_NODE_CHANNELS, server);
+				int busindex = busses.back().index;
+				(*it)->setOutputBus(server, i, busindex);
+				outputBussesRefToNode[(*it)][i] = busindex;
+			}
+		}
+		
+			for(auto &c : connections){
+				for(auto &dest : c.second){
+					int busindex = outputBussesRefToNode[c.first.getNodeRef()][c.first.getIndex()];
+					dest->setInputBus(server, c.first.getNodeRef(), busindex);
+					inputBussesRefToNode[dest].push_back(busindex);
+				}
+			}
+		
+		for (auto it = newNodesList.rbegin(); it != newNodesList.rend(); ++it) {
+			(*it)->reassignAudioControls.notify();
+		}
+		
+		nodesList = newNodesList;
 //            nodesList.insert(nodesList.end(), newNodesList.begin(), newNodesList.end());
 //        }
 //        server->sendStoredBundle();
 //        server->setWaitToSend(false);
-        server->setBLatency(false);
-    }
-    graphComputed.notify();
+		server->setBLatency(false);
+	}
+	graphComputed.notify();
 }
 
 //int serverManager::getOutputBusForNode(scNode* node){
--- src/serverManager.h
+++ src/serverManager.h
@@ -22,102 +22,115 @@ class scNode;
 class scStart;
 class scOutput;
 
+// Forward declare VST event structures
+struct VSTParameterEvent;
+struct VSTPluginOpenedEvent;
+
 struct scPreferences{
-    bool local = true;
-    int udpPort = 57110; //u (0-65535)
-    std::string bindAddress = "127.0.0.1"; //-B (set to 0.0.0.0 to listen to all)
-    int numControlBusChannels = 16384; //c
-    int numAudioBusChannels = 4096; //a
-    int numInputBusChannels = 2; //i
-    int numOutputBusChannels = 16; //o
-    int blockSize = 64; //z
-    int hardwareBufferSize = 64; //Z
-    int hardwareSampleRate = 44100; //S
-    int numBuffers = 1024; //b
-    int maxNodes = 1024; //n
-    int maxSynthDefs = 1024; //d
-    int memSize = 2048576; //m
-    int numWireBufs = 8192; //w
-    int numRGens = 64; //r
-    int maxLogins = 1; //l
-    float safetyClipThreshold = 1.26; //s
-    std::string deviceName = "nil"; //H
-    int verbosity = 0;
-    std::string ugensPlugins = ""; //-U (list of paths separated by :)
+	bool local = true;
+	int udpPort = 57110; //u (0-65535)
+	std::string bindAddress = "127.0.0.1"; //-B (set to 0.0.0.0 to listen to all)
+	int numControlBusChannels = 16384; //c
+	int numAudioBusChannels = 4096; //a
+	int numInputBusChannels = 2; //i
+	int numOutputBusChannels = 16; //o
+	int blockSize = 64; //z
+	int hardwareBufferSize = 64; //Z
+	int hardwareSampleRate = 44100; //S
+	int numBuffers = 1024; //b
+	int maxNodes = 1024; //n
+	int maxSynthDefs = 1024; //d
+	int memSize = 2048576; //m
+	int numWireBufs = 8192; //w
+	int numRGens = 64; //r
+	int maxLogins = 1; //l
+	float safetyClipThreshold = 1.26; //s
+	std::string deviceName = "nil"; //H
+	int verbosity = 0;
+	std::string ugensPlugins = ""; //-U (list of paths separated by :)
 };
 
 class serverManager{
 public:
-    serverManager();
-    ~serverManager();
-    
-    void setup();
-    void draw();
-    
-    void boot();
-    void initialize();
-    void kill();
-    void loadDefs();
-    
-    void setVolume(float volume);
-    void setDelay(int delay);
-    
-    void setStereoMix(bool stereomix);
-    void setStereoMixSize(int stereomixSize);
-    
-    void setOutputChannel(int channel);
-    void recomputeGraph();
-    
-    void addOutput(scOutput* output);
-    void removeOutput(scOutput* output);
-    
-    void setAudioDevices(std::vector<std::string> audioDevices){audioDeviceNames = audioDevices;}
-    
-    ofxSCServer* getServer(){return server;}
+	serverManager();
+	~serverManager();
+	
+	void setup();
+	void draw();
+	
+	void boot();
+	void initialize();
+	void kill();
+	void loadDefs();
+	
+	void setVolume(float volume);
+	void setDelay(int delay);
+	
+	void setStereoMix(bool stereomix);
+	void setStereoMixSize(int stereomixSize);
+	
+	void setOutputChannel(int channel);
+	void recomputeGraph();
+	
+	void addOutput(scOutput* output);
+	void removeOutput(scOutput* output);
+	
+	void setAudioDevices(std::vector<std::string> audioDevices){audioDeviceNames = audioDevices;}
+	
+	ofxSCServer* getServer(){return server;}
 //    int getOutputBusForNode(scNode* node);
-    
-    void recomputeGraphOnce(){
-        numRecomputeGraphOnce++;
-        if(numRecomputeGraphOnce == outputs.size()){
-            recomputeGraph();
-            numRecomputeGraphOnce = 0;
-        }
-    }
-    
-    void resetRecomputeGraphOnce(){
-        numRecomputeGraphOnce = 0;
-    }
-    
-    scPreferences preferences;
-    ofEvent<void> graphComputed;
+	
+	void recomputeGraphOnce(){
+		numRecomputeGraphOnce++;
+		if(numRecomputeGraphOnce == outputs.size()){
+			recomputeGraph();
+			numRecomputeGraphOnce = 0;
+		}
+	}
+	
+	void resetRecomputeGraphOnce(){
+		numRecomputeGraphOnce = 0;
+	}
+	
+	scPreferences preferences;
+	ofEvent<void> graphComputed;
+	
+	// VST Events - forwarded from server
+	ofEvent<VSTParameterEvent> vstParameterChanged;
+	ofEvent<VSTParameterEvent> vstParameterAutomated;
+	ofEvent<VSTPluginOpenedEvent> vstPluginOpened;
+	
 private:
+	// VST event handlers
+	void onVSTParameterChanged(VSTParameterEvent& event);
+	void onVSTParameterAutomated(VSTParameterEvent& event);
 
-    std::vector<scNode*> connectedNodes; //List of all nodes
-    
-    ofEventListeners listeners;
-    
-    ofxSCSynth *synth;
-    ofxSCServer *server;
-    
-    std::vector<scOutput*> outputs;
-    std::vector<scNode*> nodesList;
-    std::map<scNode*, std::map<int, std::vector<ofxSCSynth*>>> synthMap;
-    
-    std::vector<ofxSCBus> busses;
-    std::map<scNode*, std::map<int, int>> outputBussesRefToNode;
-    std::map<scNode*, std::vector<int>> inputBussesRefToNode;
-    
-    float volume;
-    bool mute;
-    int delay;
-    bool stereomix;
-    int stereomixSize;
-    int audioDevice;
-    bool dumpOsc;
-    std::vector<std::string> audioDeviceNames;
-    scStart* sc;
-    
-    int numRecomputeGraphOnce;
+	std::vector<scNode*> connectedNodes; //List of all nodes
+	
+	ofEventListeners listeners;
+	
+	ofxSCSynth *synth;
+	ofxSCServer *server;
+	
+	std::vector<scOutput*> outputs;
+	std::vector<scNode*> nodesList;
+	std::map<scNode*, std::map<int, std::vector<ofxSCSynth*>>> synthMap;
+	
+	std::vector<ofxSCBus> busses;
+	std::map<scNode*, std::map<int, int>> outputBussesRefToNode;
+	std::map<scNode*, std::vector<int>> inputBussesRefToNode;
+	
+	float volume;
+	bool mute;
+	int delay;
+	bool stereomix;
+	int stereomixSize;
+	int audioDevice;
+	bool dumpOsc;
+	std::vector<std::string> audioDeviceNames;
+	scStart* sc;
+	
+	int numRecomputeGraphOnce;
 };
 
 #endif /* serverManager_h */
